747716aab3bb4e218277f9e0f9ff4e03a211bdb8
Default_Error <defolt1213@gmail.com>
2023-11-05 21:41:06 +0300

    Fixed file saving

--- src/FileSave.java
+++ src/FileSave.java
@@ -5,41 +5,42 @@ import java.io.FileWriter;
 import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.table.DefaultTableModel;
-
 public class FileSave extends JFrame {
-    public FileSave(String str, DefaultTableModel table){
+    public FileSave(String str, DefaultTableModel table) {
         FileDialog fileDialog = new FileDialog(this, str, FileDialog.SAVE);
-        String dataFileLocation = "C:/Users/darin/eclipse-workspace/Lab2/src/data/data.txt";
-        File dataFile = new File(dataFileLocation);
-        fileDialog.setDirectory(dataFile.getParent());
-        fileDialog.setFile(dataFile.getName());
+
+        String fileName = "data.txt";
+
+        fileDialog.setFile(fileName);
         fileDialog.setVisible(true);
 
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(dataFile.getAbsolutePath()));
-            int tableRowCount = table.getRowCount();
-            int tableColumnCount = table.getColumnCount();
-            for (int i = 0; i < tableRowCount; i++) {
-                for (int j = 0; j < tableColumnCount; j++) {
-                    Object cellValue = table.getValueAt(i, j);
-                    if (cellValue != null) {
-                        writer.write(cellValue.toString());
-                    } else {
-                        writer.write("");
-                    }
+        if (fileDialog.getFile() != null) {
+            File dataFile = new File(fileDialog.getDirectory(), fileDialog.getFile());
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(dataFile.getAbsolutePath()));
+                int tableRowCount = table.getRowCount();
+                int tableColumnCount = table.getColumnCount();
+                for (int i = 0; i < tableRowCount; i++) {
+                    for (int j = 0; j < tableColumnCount; j++) {
+                        Object cellValue = table.getValueAt(i, j);
+                        if (cellValue != null) {
+                            writer.write(cellValue.toString());
+                        } else {
+                            writer.write("");
+                        }
 
-                    if (j < tableColumnCount - 1) {
-                        writer.write("|");
+                        if (j < tableColumnCount - 1) {
+                            writer.write("|");
+                        }
+                    }
+                    if (i < tableRowCount - 1) {
+                        writer.write("\r\n");
                     }
                 }
-                if (i < tableRowCount - 1) {
-                    writer.write("\r\n");
-                }
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
     }
 }
